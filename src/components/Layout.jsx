d3b87429d9d7e658e3946cb649365ad99d883d82
stojanov-d <stojanovdavid3@gmail.com>
2024-04-04 16:33:16 +0200

    GamesList layout draft

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
+import GamesList from './games-list/GamesList';
 import Sidebar from './sidebar/Sidebar';
 
 function Layout() {
 	return (
-		<div className="grid grid-cols-6 gap-4">
+		<div className="grid grid-cols-6">
 			<Sidebar />
+			<GamesList />
 		</div>
 	);
 }
---
+++ src/components/games-list/GamesListItem.jsx
@@ -0,0 +1,9 @@
+function GamesListItem() {
+	return (
+		<a href="">
+			<li className="border bg-purple-500">Test</li>
+		</a>
+	);
+}
+
+export default GamesListItem;
