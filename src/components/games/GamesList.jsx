8b309f9505a4ffe2e805120c6633f646fd731b23
stojanov-d <stojanovdavid3@gmail.com>
2024-05-07 23:01:15 +0200

    Basic setup for fetching NBA games by date

--- src/components/games/GamesList.jsx
+++ src/components/games/GamesList.jsx
@@ -1,13 +1,40 @@
 import GamesListItem from './GamesListItem';
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from 'react-datepicker';
+import { useState, useEffect } from 'react';
+
+const apiKey = import.meta.env.VITE_NBA_API_KEY;
+let currentDate = new Date().toISOString().slice(0, 10);
 
 function GamesList() {
+	const [selectedDate, setSelectedDate] = useState(currentDate);
+	const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			`https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/${selectedDate}?key=${apiKey}`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setGames(data);
+			});
+	}, [selectedDate]);
+
 	return (
-		<ul className="content-center">
-			<GamesListItem itemContent={'Game1'} />
-			<GamesListItem itemContent={'Game2'} />
-			<GamesListItem itemContent={'Game3'} />
-			<GamesListItem itemContent={'Game4'} />
-		</ul>
+		<div>
+			<DatePicker
+				defaultValue={currentDate}
+				selected={selectedDate}
+				onChange={(date) => setSelectedDate(date)}
+			/>
+			<ul className="content-center">
+				<GamesListItem itemContent={'Game1'} />
+				<GamesListItem itemContent={'Game2'} />
+				<GamesListItem itemContent={'Game3'} />
+				<GamesListItem itemContent={'Game4'} />
+			</ul>
+		</div>
 	);
 }
 
